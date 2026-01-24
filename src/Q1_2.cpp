c48978d1915703bd690e6f67435427bc98637d6e
Diana <40523965+dianas11xx@users.noreply.github.com>
2026-01-23 21:24:01 -0500

    Add files via upload

---
+++ src/Q1_2.cpp
@@ -0,0 +1,19 @@
+#include "Q1_2.h"
+
+
+vector<int> Q1_2::dfs(vector<vector<char>>& graph){
+    int n = graph.size();
+    seen = vector(n, false);
+    seen[0] = true;
+    // map the graph to integers
+    for(vector<char> edge: graph){
+
+    }
+    dfs_helper(graph, 'a');
+    return ans;
+
+}
+void Q1_2::dfs_helper(vector<vector<char>>& graph, char node){
+    
+}
+
